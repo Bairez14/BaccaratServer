22492b2c3710c001be2f9d788d3c949b3edcd29a
Bairez14 <Bairez14@users.noreply.github.com>
2021-11-22 21:16:44 -0600

    updated
    Co-authored-by: xlzth <xlzth@users.noreply.github.com>

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -60,7 +60,7 @@ public class Server{
 			int count;
 			ObjectInputStream in;
 			ObjectOutputStream out;
-			BaccaratGame game = new BaccaratGame(); //only creates one instance
+			BaccaratGame game;// //only creates one instance of game
 			
 			ClientThread(Socket s, int count){
 				this.connection = s;
@@ -93,33 +93,66 @@ public class Server{
 					
 				 while(true) { // start Baccarat stuff here??
 					    try {
-
+							// System.out.println("inside try loop, beginning");
 							BaccaratInfo clientInfo = (BaccaratInfo)in.readObject();
-							//add cards that were genereated by dealer to clientinfo
-							clientInfo.playerHand.add(game.playerHand.get(0).toString());
-							clientInfo.playerHand.add(game.playerHand.get(1).toString());
-							clientInfo.bankerHand.add(game.bankerHand.get(0).toString());
-							clientInfo.bankerHand.add(game.bankerHand.get(1).toString());
+							game = new BaccaratGame();
+							// System.out.println(clientInfo);
 							game.betOn = clientInfo.betOnWho;
 							game.currentBet = clientInfo.currentBet;
-							game.winner = clientInfo.winner;
-							game.totalWinnings = clientInfo.totalEarnings;
-							game.pdraw = clientInfo.playerDrew;
-							game.bdraw = clientInfo.bankerDrew;
+							clientInfo.currentEarnings = game.evaluateWinnings();
+							//add cards that were genereated by dealer to clientinfo
+							// System.out.println("got here before cards");
+							// clientInfo.playerHand = new ArrayList<String>();
+							// clientInfo.bankerHand = new ArrayList<String>();
+							System.out.println("Player Cards:");
+							for(Card c: game.playerHand){
+								System.out.println(c.getSuite() + c.getValue());
+								clientInfo.playerHand.add(c.toString()); 
+							}
+							System.out.println("Banker cards: ");
+							for(Card c: game.bankerHand){
+								
+								System.out.println(c.getSuite() + c.getValue());
+								clientInfo.bankerHand.add(c.toString());
+								
+							}
+							//System.out.println("did cards");
+
+							// clientInfo.playerHand.add(game.playerHand.get(0).toString());
+							// System.out.println("got here");
+							// clientInfo.playerHand.add(game.playerHand.get(1).toString());
+							// System.out.println("got here 2");
+							// clientInfo.bankerHand.add(game.bankerHand.get(0).toString());
+							// clientInfo.bankerHand.add(game.bankerHand.get(1).toString());
+
+						
+							//System.out.println("got here");
+							//game.currentBet = clientInfo.currentBet;
+							//game.winner = clientInfo.winner;
+							clientInfo.winner = game.winner;
+							clientInfo.totalEarnings = game.totalWinnings;
+							clientInfo.currentEarnings = game.currentBet;
+							clientInfo.playerDrew = game.pdraw;
+							clientInfo.bankerDrew = game.bdraw;
+							clientInfo.pPoints = BaccaratGameLogic.playerPoints;
+							clientInfo.bPoints = BaccaratGameLogic.bankerPoints;
+							// game.totalWinnings = clientInfo.totalEarnings;
+							// game.pdraw = clientInfo.playerDrew;
+							// game.bdraw = clientInfo.bankerDrew;
 							// 0 = draw
 							// 1 = player
 							// 2 = banker
 							// -1 = no natural win
-							game.natural = clientInfo.naturalWin;
-					    	 
-							callback.accept("Client: " + count + " bet on: " + clientInfo.betOnWho);
+							//game.natural = clientInfo.naturalWin;
+							clientInfo.naturalWin = game.natural;
+							callback.accept("Client: " + count + " bet on: " + clientInfo.betOnWho + " for " + clientInfo.currentEarnings);
 							callback.accept("Actual winner: " + game.winner);
-							callback.accept("Client: " + count + " earned: $" + game.totalWinnings);
-							
+							callback.accept("Natural win: " + clientInfo.naturalWin);
+							callback.accept("Client: " + count + " earned: $" + clientInfo.totalEarnings);
 							out.writeObject(clientInfo);
-					    	}
-					    catch(Exception e) {
-					    	callback.accept("Client #"+count+" has left the server!");
+					    }catch(Exception e) {
+							callback.accept("Client #" + count + " has left the server!");
+							e.printStackTrace();
 					    	clients.remove(this);
 					    	break;
 					    }
